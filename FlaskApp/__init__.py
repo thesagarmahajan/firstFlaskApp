737a0f8252fcec5eed6a276921ba9656bf7f0d06
sagar <sagarmahajan31997@gmail.com>
2020-12-12 00:08:30 +0530

    Completed fetching data from post request through form-data, x-www-form-urlencoded and raw JSON and returning in JSON format

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+db = SQLAlchemy()
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:blue'>Hello Motherfuckers!</h1>"
 
-@app.route("/timepass")
-def timepass():
-	return "<h1>This is timepass</h1>"
+@app.route("/postRawJson", methods=["POST"])
+def postRawJson():
+    return jsonify(request.json)
+
+@app.route("/postFormData", methods=["POST"])
+def postFormData():
+    print(request.form.to_dict())
+    return jsonify(request.form.to_dict())
+
+@app.route("/x_www_form_urlencoded", methods=["POST"])
+def x_www_form_urlencoded():
+    print(request.form.to_dict())
+    return jsonify(request.form.to_dict())
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=9090, debug=True)
