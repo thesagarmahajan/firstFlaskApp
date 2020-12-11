cf668c8d186ea1a8f19673a2775be3434a57ff15
sagar <sagarmahajan31997@gmail.com>
2020-12-12 00:12:06 +0530

    Comments added before functions

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -8,15 +8,22 @@ db = SQLAlchemy()
 def hello():
     return "<h1 style='color:blue'>Hello Motherfuckers!</h1>"
 
+#  Send data using POST method and Selecting raw option in body tab of
+#  Postman API Testing tool
 @app.route("/postRawJson", methods=["POST"])
 def postRawJson():
     return jsonify(request.json)
 
+#  Send data using POST method and Selecting form-data option in body tab of
+#  Postman API Testing tool
 @app.route("/postFormData", methods=["POST"])
 def postFormData():
     print(request.form.to_dict())
     return jsonify(request.form.to_dict())
 
+
+#  Send data using POST method and Selecting x-www-form-urlencoded option in body tab of
+#  Postman API Testing tool
 @app.route("/x_www_form_urlencoded", methods=["POST"])
 def x_www_form_urlencoded():
     print(request.form.to_dict())
