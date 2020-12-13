11ae54e3fe5642d7748e69ad1943abc501f7be90
sagar mahajan <sagar@techfryday.com>
2020-12-13 23:32:43 +0530

    Completing modulesexample i.e. importing class from a file inside another folder and calling functions inside the class by creating it's object

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -1,13 +1,52 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+from some_other_folder.some_other_class import Some_other_class
+
+
+app = Flask(__name__)
+db = SQLAlchemy()
+    
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return jsonify({"Status":"SUCCESS", "message":"Welcome to REST api with Flask"})
 
-@app.route("/timepass")
-def timepass():
-	return "<h1>This is timepass</h1>"
+#  Send data using POST method and Selecting raw option in body tab of
+#  Postman API Testing tool
+@app.route("/postRawJson", methods=["POST"])
+def postRawJson():
+    return jsonify(request.json)
+
+#  Send data using POST method and Selecting form-data option in body tab of
+#  Postman API Testing tool
+@app.route("/postFormData", methods=["POST"])
+def postFormData():
+    print(request.form.to_dict())
+    return jsonify(request.form.to_dict())
+
+
+#  Send data using POST method and Selecting x-www-form-urlencoded option in body tab of
+#  Postman API Testing tool
+@app.route("/x_www_form_urlencoded", methods=["POST"])
+def x_www_form_urlencoded():
+    print(request.form.to_dict())
+    return jsonify(request.form.to_dict())
+
+
+# This function returns the HTML template!
+@app.route("/home_page")
+def home_page():
+    return render_template('pages/sampleInnerPage.html')
+
+# Calling functions from user created python classes existing in different python files
+@app.route("/moduleExample")
+def moduleExample():
+    ocObj = Some_other_class()
+    return ocObj.sample_function1()
+
+# Creating object dynamically of a class in different and call the class function requested from the url and pass parameters to the function 
+# URL Format: /controller_name/function_name/param1/param2/...../param_n
+# This function will update soon...stay tuned!!!
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=9090, debug=True)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# My first flask app on 'GitHub'
+# This project will helo you to warmup with Flask
+What to remember:
+1.store html files in templates folder. render_template will find templates inside the templates folder by default.
